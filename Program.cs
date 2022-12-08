108a161d375efed923353c55481b46460fdffc3b
Martin Holman <me@martinholman.co.nz>
2022-12-08 12:29:48 -0800

    Add honeycomb tracing

--- Program.cs
+++ Program.cs
@@ -1,15 +1,41 @@
+using OpenTelemetry;
 using Microsoft.EntityFrameworkCore;
+using OpenTelemetry.Trace;
+using Honeycomb.OpenTelemetry;
 
 var builder = WebApplication.CreateBuilder(args);
+var honeycombOptions = builder.Configuration.GetHoneycombOptions();
+
+// Setup OpenTelemetry Tracing
+builder.Services.AddOpenTelemetryTracing(otelBuilder =>
+    otelBuilder
+        .AddHoneycomb(honeycombOptions)
+        .AddAutoInstrumentations()
+        .AddProcessor<BaggageSpanProcessor>()
+);
+
+// Register Tracer so it can be injected into other components (eg Controllers)
+builder.Services.AddSingleton(TracerProvider.Default.GetTracer(honeycombOptions.ServiceName));
+
 builder.Services.AddDbContext<BeeDb>(opt => opt.UseInMemoryDatabase("bees"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+
 var app = builder.Build();
+
 
 app.UseSwagger();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", (Tracer tracer) =>
+{
+    Baggage.Current.SetBaggage("app.username", "martin");
+    using var span = tracer.StartActiveSpan("app.manual-span");
+    span.SetAttribute("app.manual-span.message", "Adding custom spans is also super easy!");
+});
 
 app.MapGet("/bees", async (BeeDb db) =>
     await db.Bees.ToListAsync());
@@ -55,4 +81,4 @@ app.MapDelete("/bees/{id}", async (int id, BeeDb db) =>
 
 app.UseSwaggerUI();
 
-app.Run();
+await app.RunAsync();
